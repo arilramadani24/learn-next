63623482d2eed64e16b9b14e90a5e50f97442b26
arilramadani24 <arilramadani245@gmail.com>
2023-01-05 12:11:48 +0700

    add login page & set as landing page

---
+++ pages/dashboard.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <>
+      <Layout title={"Admin Page | Dashboard"}>
+        <h2>Hello</h2>
+      </Layout>
+    </>
+  );
+}
